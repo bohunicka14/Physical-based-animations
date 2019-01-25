c65b20e4bb07b67a115c580790fe9b0721e12340
Inka <bohunicka.ingrid@gmail.com>
2019-01-25 22:49:02 +0100

    voronoi regions added

--- projekt.py
+++ projekt.py
@@ -15,15 +15,53 @@ def multiply(m, array):
         array[i + 1] = result_y
     return array
 
-class VoronoiRegion:
+def unit_vector(vector):
+    length = math.sqrt(vector[0]**2 + vector[1]**2)
+    return [vector[0] / length, vector[1] / length]
 
-    def __init__(self, canvas, vertex, endpoint):
-        self.canvas = canvas
-        self.vertex = vertex
-        self.endpoint = endpoint
+def ray_line_segment_intersection(o, d, a, b):
+    ## returns true if ray intersects with line segment
+    ## o - point of ray
+    ## d - dir vector of ray
+    ## a,b - line segment points
+    ortho1 = [-d[1], d[0]]
+    ortho2 = [d[1], -d[0]]
+    a_to_o = [o[0] - a[0], o[1] - a[1]]
+    a_to_b = [b[0] - a[0], b[1] - a[1]]
 
-    def draw(self):
-        self.canvas.create_line(self.vertex + self.endpoint, fill = 'red')
+    denom1 = a_to_b[0] * ortho1[0] + a_to_b[1] * ortho1[1]
+    if denom1 == 0:
+        return False
+
+    denom2 = a_to_b[0] * ortho2[0] + a_to_b[1] * ortho2[1]
+    if denom2 == 0:
+        return False
+
+    t1 = abs(a_to_b[0] * a_to_o[1] - a_to_o[0] * a_to_b[1]) / denom1
+    t2 = (a_to_o[0] * ortho1[0] + a_to_o[1] * ortho1[1]) / denom1
+
+    t12 = abs(a_to_b[0] * a_to_o[1] - a_to_o[0] * a_to_b[1]) / denom2
+    t22 = (a_to_o[0] * ortho2[0] + a_to_o[1] * ortho1[1]) / denom2
+
+    partial_result1 = t2 >= 0 and t2 <= 1 and t1 >= 0
+    partial_result2 = t22 >= 0 and t22 <= 1 and t12 >= 0
+
+
+    return partial_result1 and partial_result2
+
+
+# class VoronoiRegion:
+#
+#     def __init__(self, canvas, vertex1, endpoint1, vertex2, endpoint2):
+#         self.canvas = canvas
+#         self.vertex1 = vertex1
+#         self.endpoint1 = endpoint1
+#         self.vertex2 = vertex2
+#         self.endpoint2 = endpoint2
+#
+#     def draw(self):
+#         self.canvas.create_line(self.vertex1 + self.endpoint1, fill = 'red')
+#         self.canvas.create_line(self.vertex2 + self.endpoint2, fill = 'red')
 
 
 class Feature():
@@ -36,23 +74,42 @@ class Feature():
 
 class Vertex(Feature):
 
-    def __init__(self, x, y, marked = False):
+    def __init__(self, canvas, x, y, marked = False):
+        self.canvas = canvas
         self.x = x
         self.y = y
         self.marked = marked
-        self.voronoi_region = []
+        self.voronoi_region = [[self.x, self.y], [self.x, self.y]]
+        self.vr1 = self.vr2 = None
 
     def update(self, x, y):
         self.x = x
         self.y = y
+        self.voronoi_region = [[self.x, self.y], [self.x, self.y]]
+
+    def draw_VR(self, color = 'blue'):
+        if self.vr2 is not None and self.vr1 is not None:
+            self.canvas.delete(self.vr1)
+            self.canvas.delete(self.vr2)
+        if len(self.voronoi_region[0]) == 4:
+            self.vr1 = self.canvas.create_line(self.voronoi_region[0], fill=color)
+        if len(self.voronoi_region[1]) == 4:
+            self.vr2 = self.canvas.create_line(self.voronoi_region[1], fill=color)
+
+    def move_vr(self, x, y):
+        self.canvas.move(self.vr1, x, y)
+        self.canvas.move(self.vr2, x, y)
+
 
 class Edge(Feature):
 
-    def __init__(self, v1, v2, marked = False):
+    def __init__(self, v1, v2, polygon, marked = False):
         self.v1 = v1
         self.v2 = v2
+        self.polygon = polygon
         self.marked = marked
-        self.voronoi_region = []
+        self.voronoi_region = self.create_voronoi_region()
+        self.vr1 = self.vr2 = None
 
     # def update(self, x1, y1, x2, y2):
     #     self.v1.update(x1, y1)
@@ -63,11 +120,56 @@ class Edge(Feature):
 
     def get_normal_vector(self):
         directional = self.get_directional_vector()
-        return [directional[1], -directional[0]]
+        normal_vector = [-directional[1], directional[0]]
+        vector = unit_vector(normal_vector)
+        mid_point = [(self.v2.x - self.v1.x) / 2 + self.v1.x, (self.v2.y - self.v1.y) / 2 + self.v1.y]
+
+        for edge in self.polygon.edges:
+            if edge != self:
+                # intersects = ray_line_segment_intersection(mid_point, vector, [edge.v1.x, edge.v1.y], [edge.v2.x, edge.v2.y])
+                intersects = self.polygon.point_in_polygon(mid_point[0] + vector[0], mid_point[1] + vector[1])
+                if intersects:
+                    return [-vector[0], -vector[1]]
+        return vector
 
     def get_inverse_normal_vector(self):
         normal = self.get_normal_vector()
         return [-normal[0], -normal[1]]
+
+    def create_voronoi_region(self):
+        vector = self.get_normal_vector()
+
+        vector[0] *= 1000
+        vector[1] *= 1000
+
+        ax = vector[0] + self.v1.x
+        ay = vector[1] + self.v1.y
+
+        bx = vector[0] + self.v2.x
+        by = vector[1] + self.v2.y
+
+        self.v1.voronoi_region[0].append(ax)
+        self.v1.voronoi_region[0].append(ay)
+
+        self.v2.voronoi_region[1].append(bx)
+        self.v2.voronoi_region[1].append(by)
+
+        return [[self.v1.x, self.v1.y, ax, ay], [self.v2.x, self.v2.y, bx, by]]
+
+    def draw_VR(self, color = 'blue'):
+        if self.vr2 is not None and self.vr1 is not None:
+            self.polygon.canvas.delete(self.vr1)
+            self.polygon.canvas.delete(self.vr2)
+        self.vr1 = self.polygon.canvas.create_line(self.voronoi_region[0], fill=color)
+        self.vr2 = self.polygon.canvas.create_line(self.voronoi_region[1], fill=color)
+
+    def move_vr(self, x, y):
+        self.polygon.canvas.move(self.vr1, x, y)
+        self.polygon.canvas.move(self.vr2, x, y)
+
+    def update(self):
+        self.voronoi_region = self.create_voronoi_region()
+
 
 class FeaturePair():
 
@@ -117,6 +219,23 @@ class PolyObject:
         else:
             self.id = self.canvas.create_polygon(*coords, fill=color, outline='black', width=1, tag=self.tag)
 
+    def point_in_polygon(self, x, y):
+        n = len(self.vertices)
+        v = False
+
+        b1x, b1y = self.vertices[0].x, self.vertices[0].y
+        for i in range(n+1):
+            b2x, b2y = self.vertices[i%n].x, self.vertices[i%n].y
+            if y > min(b1y, b2y):
+                if y <= max(b1y, b2y):
+                    if x <= max(b1x, b2x):
+                        if b1y != b2y:
+                            xin = (y-b1y)*(b2x-b1x)/(b2y-b1y) + b1x
+                        if b1x == b2x or x <= xin:
+                            v = not v
+            b1x,b1y = b2x,b2y
+        return v
+
     def set_active(self, state):
         """ Zmena outline farby
         """
@@ -132,10 +251,10 @@ class PolyObject:
             ## init features
             for i in range(0, len(self.coords), 2):
                 # print(len(self.coords), self.coords[i], self.coords[i+1])
-                self.vertices.append(Vertex(self.coords[i], self.coords[i + 1]))
+                self.vertices.append(Vertex(self.canvas, self.coords[i], self.coords[i + 1]))
             for i in range(len(self.vertices) - 1):
-                self.edges.append(Edge(self.vertices[i], self.vertices[i + 1]))
-            self.edges.append(Edge(self.vertices[0], self.vertices[-1]))
+                self.edges.append(Edge(self.vertices[i], self.vertices[i + 1], self))
+            self.edges.append(Edge(self.vertices[0], self.vertices[-1], self))
             # print(len(self.edges))
         else:
             ## update features according to new coords
@@ -143,6 +262,9 @@ class PolyObject:
             for i in range(0, len(self.coords), 2):
                 self.vertices[j].update(self.coords[i], self.coords[i+1])
                 j += 1
+
+            for e in self.edges:
+                e.update()
 
     def set_coords(self, coords):
         self.coords = coords
@@ -267,14 +389,29 @@ class Playground(Tk):
 
         self.p1.set_coords(self.playground.coords(self.p1.id))
         self.p2.set_coords(self.playground.coords(self.p2.id))
-        print('coords   ', self.p1.coords)
 
-        self.v_clip(self.p1, self.p2, None, None)
+        # # for v in self.p1.vertices:
+        # #     v.draw_VR()
+        #
+        # for e in self.p1.edges:
+        #     e.draw_VR()
+        #
+        # # for v in self.p2.vertices:
+        # #     v.draw_VR()
+        #
+        # for e in self.p2.edges:
+        #     e.draw_VR()
+
+
+        # print('coords   ', self.p1.coords)
+
+        # self.v_clip(self.p1, self.p2, None, None)
 
     def redraw_canvas(self):
         self.playground.delete('all')
         self.p1.draw(self.p1.coords, 'white')
         self.p2.draw(self.p2.coords, 'navy')
+
 
     def click(self, event):
         self.obj_id = event.widget.find_closest(event.x, event.y)[0]
@@ -297,17 +434,33 @@ class Playground(Tk):
             self.p1.set_coords(self.playground.coords(self.p1.id))
         elif self.p2.is_active:
             self.p2.set_coords(self.playground.coords(self.p2.id))
-        self.p1.get_centroid()
-        self.p2.get_centroid()
+        # self.p1.get_centroid()
+        # self.p2.get_centroid()
 
     def drag(self, event):
         # if self.obj_tag == ('object' + str(self.obj_id)):
         self.playground.move(self.obj_id, event.x - self.ex, event.y - self.ey)
         if self.p1.is_active:
             self.p1.set_coords(self.playground.coords(self.p1.id))
+            # for e in self.p1.edges:
+            #     e.move_vr(event.x - self.ex, event.y - self.ey)
+            for v in self.p1.vertices:
+                v.draw_VR()
+
+            for e in self.p1.edges:
+                e.draw_VR()
         elif self.p2.is_active:
             self.p2.set_coords(self.playground.coords(self.p2.id))
+            # for e in self.p2.edges:
+            #     e.move_vr(event.x - self.ex, event.y - self.ey)
+            for v in self.p2.vertices:
+                v.draw_VR()
+
+            for e in self.p2.edges:
+                e.draw_VR()
+
         self.ex, self.ey = event.x, event.y
+
 
     def v_clip(self, A, B, X, Y):
         # print(A, B, X, Y)
